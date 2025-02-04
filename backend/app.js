35f72ffd5acffba38da177a39df33c71a9a8e6db
Manish Patil <patilmanishp18@gmail.com>
2025-02-04 18:06:49 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
+const userRoute = require("./routes/usersRoutes");
+const citiesRoute = require("./routes/citiesRoutes");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
+
+app.use("/auth", userRoute);
+app.use("/cities", citiesRoute);
 
 app.get("/", (req, res) => {
   res.send("WORLDWISE✨");
