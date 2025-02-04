4dd06b806101a95450051e23b234d3c00b412ea0
Manish Patil <patilmanishp18@gmail.com>
2025-02-04 19:19:41 +0530

    frontend: Define all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./contexts/AuthContext";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+          <Toaster position="bottom-left" reverseOrder={false} />
+          <Routes>
+            <Route path="/" element={<h1>Homepage</h1>} />
+            <Route path="/product" element={<h1>Product</h1>} />
+            <Route path="/pricing" element={<h1>Pricing</h1>} />
+            <Route path="/auth" element={<h1>Auth</h1>} />
+            <Route path="/app" element={<h1>App</h1>}></Route>
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
