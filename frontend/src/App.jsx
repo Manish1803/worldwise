fe35fe42091729f99903fc7d7fb7ff1e3935797f
Manish Patil <patilmanishp18@gmail.com>
2025-02-08 11:43:12 +0530

    feat: Add Homepage, Product, Offer and Auth Pages with related components
    - Implement Homepage layout with Logo, PageNav components and hero section
    - Create Product and Offer page
    - Add Authentication pages (Login, Register) with form validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Offer from "./pages/Offer";
+import Auth from "./pages/Auth";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <AuthProvider>
-          <Toaster position="bottom-left" reverseOrder={false} />
+          <Toaster position="top-center" reverseOrder={false} />
           <Routes>
-            <Route path="/" element={<h1>Homepage</h1>} />
-            <Route path="/product" element={<h1>Product</h1>} />
-            <Route path="/pricing" element={<h1>Pricing</h1>} />
-            <Route path="/auth" element={<h1>Auth</h1>} />
-            <Route path="/app" element={<h1>App</h1>}></Route>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/offers" element={<Offer />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/app" element={<AppLayout />}></Route>
             <Route path="*" element={<h1>Page Not Found</h1>} />
           </Routes>
         </AuthProvider>
