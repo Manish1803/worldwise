c58814b4663a6a1a01e8fdf85d7e076ced545702
Manish Patil <patilmanishp18@gmail.com>
2025-03-01 16:17:37 +0530

    backend: (refactor) Update User model
    - Implemented automatic username generation
    - Ensured unique usernames with a validation check
    - Improved schema structure for better maintainability

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,34 +3,55 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Name is required"],
+  },
   username: {
     type: String,
-    required: [true, "Username is required"],
-    unique: [true, "Username must be unique."],
-    lowercase: [true, "Username must be lowercase."],
     trim: true,
-    validate: {
-      validator: function (value) {
-        return !/\s/.test(value);
-      },
-      message: "Username must not contain spaces.",
-    },
+    username: [true, "Username must be unique"],
   },
   email: {
     type: String,
+    trim: true,
     required: [true, "Email is required"],
     unique: [true, "Email is already in use."],
     lowercase: [true, "Email must be lowercase."],
-    trim: true,
     validate: [validator.isEmail, "Please enter a valid email address"],
   },
   password: {
     type: String,
-    required: [true, "Password is required"],
     trim: true,
+    required: [true, "Password is required"],
     minlength: [6, "Password must be at least 6 characters long."],
     select: false,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("name") || this.username) next();
+
+  let baseUsername = this.name.toLowerCase().replace(/\s+/g, "");
+  let randomNum = Math.floor(100 + Math.random() * 900);
+  let newUsername = baseUsername + randomNum;
+
+  let isUnique = false;
+  while (!isUnique) {
+    const existingUser = await mongoose
+      .model("User")
+      .findOne({ username: newUsername });
+    if (!existingUser) {
+      isUnique = true;
+    } else {
+      randomNum = Math.floor(100 + Math.random() * 900);
+      newUsername = baseUsername + randomNum;
+    }
+  }
+
+  this.username = newUsername;
+  next();
 });
 
 userSchema.pre("save", async function (next) {
